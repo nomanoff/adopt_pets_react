16340c8bda73d8c2cac19fe7021b8e42260a713d
nomanoff <aliabdulazeez01@gmail.com>
2021-07-16 22:14:34 +0900

    before handling Errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Pets!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
